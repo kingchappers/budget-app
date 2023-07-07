43d66b099f87d4225401a96f6f7cd5798777919b
Sam Chapman <sam.chapman@outlook.com>
2023-07-07 13:49:03 +0100

    Created help page

---
+++ app/help/page.tsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="w-full max-w-5xl items-center justify-between font-mono text-sm">
+        <h1 className="text-4xl font-extrabold mb-4">Welcome to the help page</h1>
+        <p>This page is awaiting content and will be filled as the budget app gains functionality</p>
+      </div>
+    </main>
+  )
+}
