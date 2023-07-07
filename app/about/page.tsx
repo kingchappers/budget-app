86c49e05a4bb2f1f842c4ed73ed1bc10f1674fb4
Sam Chapman <sam.chapman@outlook.com>
2023-07-07 14:28:37 +0100

    Added external link

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,6 +10,8 @@ export default function Home() {
         <p className="mb-5">Personally I don't like linking my credit or debit accounts to budgeting apps, both from a personal security perspective and because I find there to be problems with this. Because I do most of my spending on my credit cards but use direct debits for regular payments my ideal would be to link both to a budgeting app, however, doing this generally means that individual items are added to the app which is great but when I pay off my card via direct debit that charge is then added to the app. It then reflects poorly on the app unless there's some way of removing an individual charge, which is normally a bit of a pain.</p>
 
         <p className="mb-5">It's for this reason that I am making this app manual. I believe that manually entering payments into the app also makes the spending of money feel more real. In the future I may look into implementing the Open Banking API, but for now this isn't part of my plan while I'm trying to get the app working to meet my personal requirements.</p>
+
+        <p className="mb-5">If you came here to find out more about me you can veiw information <a className="text-blue-600" href="https://samchapman.dev/" >here</a> on my personal website.</p>
       </div>
     </main>
   )
