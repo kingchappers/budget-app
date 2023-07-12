8d61dadd2060d9c15c1eb06a0e70b773f4abdef2
Sam Chapman <sam.chapman@outlook.com>
2023-07-12 09:16:35 +0100

    Added callbacks for session and jwt

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -15,5 +15,30 @@ export const authOptions: NextAuthOptions = {
             clientSecret: GITHUB_SECRET,
         }),
     ],
+    callbacks: {
+        session: ({ session, token }) => {
+            console.log("Session Callback", { session, token });
+            return {
+                ...session,
+                user: {
+                  ...session.user,
+                  id: token.id,
+                  randomKey: token.randomKey,
+                },
+            };
+        },
+        jwt: ({ token, user }) => {
+            console.log("JWT Callback", { token, user });
+            if (user) {
+                const u = user as unknown as any;
+                return {
+                    ...token,
+                    id: u.id,
+                    randomKey: u.randomKey,
+                };
+            }
+            return token;
+        },
+    },
   };
   
