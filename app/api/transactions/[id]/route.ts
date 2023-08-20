e65b1e61e108b3694e018defa2fc3f96a8996d97
Sam Chapman <sam.chapman@outlook.com>
2023-08-20 17:21:44 +0100

    added get, patch, and delete route handlers

---
+++ app/api/transactions/[id]/route.ts
@@ -0,0 +1,91 @@
+import connectDB from "@/app/lib/connect-db";
+import { deleteTransaction, getTransaction, updateTransaction } from "@/app/lib/transaction-db";
+import { createErrorResponse } from "@/app/lib/utils";
+import { create } from "domain";
+import { RequestStore } from "next/dist/client/components/request-async-storage";
+import { NextResponse } from "next/server";
+
+export async function GET(
+    _request: Request,
+    { params }: { params: { id: string } }
+) {
+    try {
+        await connectDB();
+
+        const id = params.id;
+        const { transaction, error } = await getTransaction(id);
+
+        if (error) {
+            throw error;
+        }
+
+        let json_response = {
+            status: "success",
+            data: {
+                transaction,
+            },
+        };
+        return NextResponse.json(json_response);
+    } catch (error: any) {
+        if (typeof error === "string" && error.includes("Transaction not found")) {
+            return createErrorResponse("Transaction not found", 404);
+        }
+
+        return createErrorResponse(error.message, 500)
+    }   
+}
+
+export async function PATCH(
+    request: Request,
+    { params }: { params: { id: string } }
+) {
+    try {
+        await connectDB();
+
+        const id = params.id;
+        let body = await request.json();
+
+        const { transaction, error } = await updateTransaction(id, body);
+
+        if (error) {
+            throw error;
+        }
+
+        let json_response = {
+            status: "success",
+            data: {
+                transaction,
+            },
+        };
+        return NextResponse.json(json_response);
+    } catch (error: any) {
+        if (typeof error === "string" && error.includes("Transaction not found")) {
+            return createErrorResponse("Transaction not found", 404);
+        }
+
+        return createErrorResponse(error.message, 500);
+    }
+}
+
+export async function DELETE(
+    request: Request,
+    { params }: { params: { id: string } }
+) {
+    try {
+        await connectDB();
+
+        const id = params.id;
+        const { error } = await deleteTransaction(id)
+
+        if (error) {
+            throw error;
+        }
+
+        return new NextResponse(null, { status: 204 })
+    } catch (error: any) {
+        if (typeof error === "string" && error.includes("Transaction not found")) {
+            return createErrorResponse("Transaction not found", 404);
+        }
+        return createErrorResponse(error.message, 500);
+    }
+}
