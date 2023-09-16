230302247b8c254f120fc0da7356340a27d10e99
Sam Chapman <sam.chapman@outlook.com>
2023-09-16 14:39:49 +0100

    created income model

---
+++ app/models/Income.ts
@@ -0,0 +1,59 @@
+import {
+    ModelOptions,
+    Severity,
+    getModelForClass,
+    index,
+    post,
+    prop,
+} from "@typegoose/typegoose"
+import mongoose from "mongoose"
+
+@post<IncomeClass>("save", function (doc) {
+    if (doc) {
+        doc.id = doc._id.toString();
+        doc._id = doc.id;
+    }
+})
+@post<IncomeClass[]>(/^find/, function (docs) {
+    // @ts-ignore
+    if (this.op === "find") {
+        docs.forEach((doc) => {
+            doc.id = doc._id.toString();
+            doc._id = doc.id;
+        });
+    }
+})
+@ModelOptions({
+    schemaOptions: {
+        timestamps: true,
+        collection: "income",
+    },
+    options: {
+        allowMixed: Severity.ALLOW,
+    },
+})
+@index({ vendor: 1 })
+class IncomeClass {
+
+    @prop({required: true})
+    incomeDate: string;
+    
+    @prop({required: true})
+    company: string;
+
+    @prop({required: true })
+    ammount: number;
+
+    @prop({required: true, default: "Miscellaneous"})
+    IncomeCategory: string;
+
+    @prop({required: false})
+    notes: string;
+    
+    _id: mongoose.Types.ObjectId | string;
+
+    id: string;
+}
+
+const Income = getModelForClass(IncomeClass);
+export { Income, IncomeClass};
