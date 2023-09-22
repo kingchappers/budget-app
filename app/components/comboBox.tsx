b3d2edd90ce25a5f2d6603e792169a0d5120cdae
Sam Chapman <sam.chapman@outlook.com>
2023-09-22 11:15:34 +0100

    Updated width

--- app/components/comboBox.tsx
+++ app/components/comboBox.tsx
@@ -56,6 +56,7 @@ export function CategoryComboBox({categories}: CategoriesComboProps) {
         options={categoryList}
         isClearable={true}
         backspaceRemovesValue={true}
+        classNames={{control: () => "w-44"}}
       />
       )
 }
