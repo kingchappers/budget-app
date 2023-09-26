5422ea391ce32902a3bd942a82efa444a8b4b0a1
Sam Chapman <sam.chapman@outlook.com>
2023-09-26 15:36:21 +0100

    Created Target model

---
+++ app/models/Target.ts
@@ -0,0 +1,53 @@
+import {
+    ModelOptions,
+    Severity,
+    getModelForClass,
+    index,
+    post,
+    prop,
+} from "@typegoose/typegoose"
+import mongoose from "mongoose"
+
+@post<TargetClass>("save", function (doc) {
+    if (doc) {
+        doc.id = doc._id.toString();
+        doc._id = doc.id;
+    }
+})
+@post<TargetClass[]>(/^find/, function (docs) {
+    // @ts-ignore
+    if (this.op === "find") {
+        docs.forEach((doc) => {
+            doc.id = doc._id.toString();
+            doc._id = doc.id;
+        });
+    }
+})
+@ModelOptions({
+    schemaOptions: {
+        timestamps: true,
+        collection: "targets",
+    },
+    options: {
+        allowMixed: Severity.ALLOW,
+    },
+})
+@index({ label: 1 })
+class TargetClass {
+
+    @prop({required: true})
+    categoryName: string;
+    
+    @prop({default: true})
+    targetAmount: number;
+
+    @prop({default: true})
+    expenseTarget: boolean;
+    
+    _id: mongoose.Types.ObjectId | string;
+
+    id: string;
+}
+
+const Target = getModelForClass(TargetClass);
+export { Target, TargetClass};
