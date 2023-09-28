689e6cce77157dd089ecc86fa6a83221ecb1afe9
Sam Chapman <sam.chapman@outlook.com>
2023-09-28 09:42:09 +0100

    implemented type checking on value

--- app/targets/page.tsx
+++ app/targets/page.tsx
@@ -3,39 +3,58 @@ import TargetFormServerComponent from "../components/target-form-server";
 import { TargetFilter, getTargets } from "../lib/target-db";
 
 export default async function Home() {
-    let filter: TargetFilter = {
-        limit: 50
+    let ExpenseFilter: TargetFilter = {
+        limit: 50,
+        type: "expense"
+    }
+
+    let IncomeFilter: TargetFilter = {
+        limit: 50,
+        type: "income"
     }
     
-    let { targets, results } = await getTargets(filter)
+    let { targets: expenseTargets, results: expenseResults } = await getTargets(ExpenseFilter)
+    let { targets: incomeTargets, results: incomeResults } = await getTargets(IncomeFilter)
 
     async function action(data: FormData){
         "use server"
 
-        data.forEach((value, key) => (
+        data.forEach((value, key) => {
+            if(!value || typeof Number(value) !== "number"){
+                return;
+            }
+            
             updateTargetAction(key, {targetAmount: Number(value)}, "/with-server-actions")
-        ));
+        });
     }
 
     return(
-        <div className="container mx-auto max-w-screen-2xl p-4">            
-            <h1 className="text-2xl font-bold mb-4">Expense Targets</h1>
+        <div className="container mx-auto max-w-screen-2xl p-4"> 
 
+            <h1 className="text-2xl font-bold mb-4">Expense Targets</h1>
             <form action={action} key={Math.random()} className="items-center space-x-3 mb-4">
-                {results === 0 ? (
-                    <p className="text-center">No Targets Found</p>
+                {expenseResults === 0 ? (
+                    <p className="text-center">No Expense Targets Found</p>
                 ) : (
-                    targets?.map((target) => (
-                            <TargetFormServerComponent key={target.id} target={target} />
+                    expenseTargets?.map((expenseTarget) => (
+                        <TargetFormServerComponent key={expenseTarget.id} target={expenseTarget} />
                     ))
                 )}
-                
+                <button className="px-4 py-1 text-white rounded bg-green-500">Save</button>
+            </form>
+            
+            <h1 className="text-2xl font-bold mb-4">Income Targets</h1>
+            <form action={action} key={Math.random()} className="items-center space-x-3 mb-4">
+                {incomeResults === 0 ? (
+                    <p className="text-center">No Income Targets Found</p>
+                ) : (
+                    incomeTargets?.map((incomeTarget) => (
+                        <TargetFormServerComponent key={incomeTarget.id} target={incomeTarget} />
+                    ))
+                )}
                 <button className="px-4 py-1 text-white rounded bg-green-500">Save</button>
             </form>
 
-            <h1 className="text-2xl font-bold mb-4">Income Targets</h1>
-
         </div>
-        
     );
 }
