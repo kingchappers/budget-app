4e4e9a9aa95b5f20f1e42362f88d3ea16478724f
Sam Chapman <sam.chapman@outlook.com>
2023-10-07 15:29:54 +0100

    Finished function to update table of spending

--- app/components/time-button-group.tsx
+++ app/components/time-button-group.tsx
@@ -4,47 +4,48 @@ import * as React from 'react';
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import { getTransactionsBetweenDatesAction } from '../_transactionActions';
-import { TransactionFilter } from '../lib/transaction-db';
 import { calculateTransactionTotalAction } from '../_targetActions';
+import { useState } from "react";
 
 export default function ColorToggleButton() {
-  const [alignment, setAlignment] = React.useState('web');
+  const [timeTotal, setTimeTotal] = useState(0);
+  const [alignment, setAlignment] = React.useState('week');
+  var transactionTotal = 0
+
+  setTable("week")
+
+  async function setTable(value: string){
+    if(value === "week"){
+      let endDate = new Date();
+      const startDate = new Date(endDate.setDate(endDate.getDate() - 7));
+      endDate = new Date()
+      const {transactions, results} = await getTransactionsBetweenDatesAction({startDate, endDate})
+      transactionTotal = await calculateTransactionTotalAction({transactions, results}) ?? 0
+  } else if(value === "month"){
+      const date = new Date();
+      const startDate = new Date(date.getFullYear(), date.getMonth(), 1);
+      const endDate = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+      const {transactions, results} = await getTransactionsBetweenDatesAction({startDate, endDate})
+      transactionTotal = await calculateTransactionTotalAction({transactions, results}) ?? 0
+  } else if(value === "year"){
+      const currentYear = new Date().getFullYear();
+      const startDate = new Date(currentYear, 0, 1);
+      const endDate = new Date(currentYear, 11, 31);
+      const {transactions, results} = await getTransactionsBetweenDatesAction({startDate, endDate})
+      transactionTotal = await calculateTransactionTotalAction({transactions, results}) ?? 0
+  } else {
+      transactionTotal = 0;
+  }
+
+  setTimeTotal(transactionTotal);
+  }
 
   async function handleChange (
     event: React.MouseEvent<HTMLElement>,
     value: string,
   ) {
     setAlignment(value);
-
-    if(value === "week"){
-        let endDate = new Date();
-        const startDate = new Date(endDate.setDate(endDate.getDate() - 7));
-        endDate = new Date()
-        const {transactions, results} = await getTransactionsBetweenDatesAction({startDate, endDate})
-        let total = await calculateTransactionTotalAction({transactions, results}) 
-        console.log(total)
-        // return(total);
-    } else if(value === "month"){
-        const date = new Date();
-        const startDate = new Date(date.getFullYear(), date.getMonth(), 1);
-        const endDate = new Date(date.getFullYear(), date.getMonth() + 1, 0);
-        const {transactions, results} = await getTransactionsBetweenDatesAction({startDate, endDate})
-        const total = await calculateTransactionTotalAction({transactions, results})
-        console.log(total)
-        // return(total);
-    } else if(value === "year"){
-        const currentYear = new Date().getFullYear();
-        const startDate = new Date(currentYear, 0, 1);
-        const endDate = new Date(currentYear, 11, 31);
-        const {transactions, results} = await getTransactionsBetweenDatesAction({startDate, endDate})
-        const total = await calculateTransactionTotalAction({transactions, results})
-        console.log(total)
-        // return(total);
-    } else {
-        const total = 0;
-        console.log(total)
-        // return(total);
-    }
+    setTable(value)
   };
 
   return (
@@ -61,7 +62,7 @@ export default function ColorToggleButton() {
         <ToggleButton value="year">Year</ToggleButton>
         </ToggleButtonGroup>
 
-        <p></p>
+        <p>{timeTotal}</p>
     </div>
   );
 }
