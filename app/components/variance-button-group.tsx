e4bf3ea14dad8de0890b0979d1f4e9b53fbd6070
Sam Chapman <sam.chapman@outlook.com>
2023-10-11 15:30:27 +0100

    function update and started formating

--- app/components/variance-button-group.tsx
+++ app/components/variance-button-group.tsx
@@ -6,150 +6,181 @@ import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import { getTransactionsBetweenDatesAction } from '../_transactionActions';
 import { calculateTransactionTotalAction, getTargetsAction } from '../_targetActions';
 import { useState } from "react";
-import VarianceTable from "./variance-table-server"
+import TargetItem from './target-form-server';
 
-export default function timeButton() {
-  React.useEffect(() => {
-    setTable("week")
-    setTargetItems()
-  },[])
+let didInit = false;
 
-  interface targetItem {
-    targetName?: string;
-    targetValue?: number;
-    actualSpent?: number;
-  }
+interface targetItem {
+    targetName: string;
+    targetValue: number;
+    targetType: boolean;
+    actualSpend: number;
+}
 
-  const [timeTransactionTotal, setTimeTransactionTotal] = useState(0);
-  const [alignment, setAlignment] = React.useState('week');
-  var targetList: targetItem = {}
-  const [targetListItem, setTargetListItem] = useState(targetList)
-  var transactionTotal = 0
+var targetItems: targetItem[] = []
 
-  async function setTargetItems() {
-    let endDate = new Date();
-    const startDate = new Date(endDate.setDate(endDate.getDate() - 7));
-    endDate = new Date()
-    var tests: targetItem[] = []
+export default function varianceTimeButton() {
+    React.useEffect(() => {
+        //The if checks if the page has already initialised and stops it running twice
+        if (!didInit) {
+            didInit = true;
+            setTable("week")
+        }
+    }, [])
 
-    var { transactions, results: transactionResults} = await getTransactionsBetweenDatesAction({startDate, endDate})
-    var { targets, results: targetResults } = await getTargetsAction()
 
-    // console.log(transactions?.find((transaction) => {return transaction.category === "Childcare"}))
-    
-    targets?.forEach((target) => {
-      // console.log(target.categoryName)
-      // console.log(target.expenseTarget)
-      // console.log(target.targetAmount)
+    const [timeTransactionTotal, setTimeTransactionTotal] = useState(0);
+    const [alignment, setAlignment] = React.useState('week');
+    var transactionTotal = 0
 
-      const newItem: targetItem = {
-        targetName: target.categoryName,
-        targetValue: target.targetAmount,
-        actualSpent: 0
-      }
+    const [test, setTest] = useState(targetItems)
 
-      tests.push(newItem)
-    })
+    async function setTargetItems() {
+        targetItems = []
+        console.log(targetItems)
+        let endDate = new Date();
+        const startDate = new Date(endDate.setDate(endDate.getDate() - 7));
+        endDate = new Date()
 
-    console.log("this is the test variable:" + tests)
+        var { transactions, results: transactionResults } = await getTransactionsBetweenDatesAction({ startDate, endDate })
+        var { targets, results: targetResults } = await getTargetsAction()
 
-    transactions?.forEach((transaction) => {
-      console.log(transaction.category)
-      const foo = tests.find((test) => {return test.targetName === transaction.category})
-      const index = tests.findIndex((test) => {return test.targetName === transaction.category})
-      console.log(foo)
-      console.log(index)
+        // console.log(transactions?.find((transaction) => {return transaction.category === "Childcare"}))
 
-      tests[index].actualSpent = tests[index].actualSpent + transaction.value
+        targets?.forEach((target) => {
+            // console.log(target.categoryName)
+            // console.log(target.expenseTarget)
+            // console.log(target.targetAmount)
 
-      console.log(tests[index])
-    })
+            const newItem: targetItem = {
+                targetName: target.categoryName,
+                targetValue: target.targetAmount,
+                targetType: target.expenseTarget,
+                actualSpend: 0
+            }
 
-    
-    
-  //   {results === 0 ? (
-  //     <p className="text-center">No Transactions Found</p>
-  // ) : (
-  //     transactions?.map((transaction) => (
-  //         <TransactionItemServerComponent key={transaction.id} transaction={transaction} />
-  //     ))
-  // )}
-    
+            targetItems.push(newItem)
+        })
 
-  //   data.forEach((value, key) => {
-  //     if(!value || typeof Number(value) !== "number"){
-  //         return;
-  //     }
-      
-  //     updateTargetAction(key, {targetAmount: Number(value)}, "/with-server-actions")
-  // });
+        // console.log("this is the test variable:" + targetItems)
 
-  }
+        transactions?.forEach((transaction) => {
+            // console.log(transaction.category)
+            const foo = targetItems.find((targetItem) => { return targetItem.targetName === transaction.category })
+            const index: number = targetItems.findIndex((targetItem) => { return targetItem.targetName === transaction.category })
+            // console.log(foo)
+            // console.log(index)
 
-  async function setTable(value: string){
-    if(value === "week"){
-      let endDate = new Date();
-      const startDate = new Date(endDate.setDate(endDate.getDate() - 7));
-      endDate = new Date()
-      const {transactions, results} = await getTransactionsBetweenDatesAction({startDate, endDate})
-      transactionTotal = await calculateTransactionTotalAction({transactions, results}) ?? 0
-    } else if(value === "month"){
-      const date = new Date();
-      const startDate = new Date(date.getFullYear(), date.getMonth(), 1);
-      const endDate = new Date(date.getFullYear(), date.getMonth() + 1, 0);
-      const {transactions, results} = await getTransactionsBetweenDatesAction({startDate, endDate})
-      transactionTotal = await calculateTransactionTotalAction({transactions, results}) ?? 0
-    } else if(value === "year"){
-      const currentYear = new Date().getFullYear();
-      const startDate = new Date(currentYear, 0, 1);
-      const endDate = new Date(currentYear, 11, 31);
-      const {transactions, results} = await getTransactionsBetweenDatesAction({startDate, endDate})
-      transactionTotal = await calculateTransactionTotalAction({transactions, results}) ?? 0
-    } else {
-      transactionTotal = 0;
+            targetItems[index].actualSpend = targetItems[index].actualSpend + transaction.value
+
+            console.log("boo")
+            console.log(targetItems.length)
+
+        })
+
+        setTest(targetItems)
+
+        // console.log(test)
+
     }
 
-    setTimeTransactionTotal(transactionTotal);
-  }
+    async function setTable(value: string) {
+        if (value === "week") {
+            let endDate = new Date();
+            const startDate = new Date(endDate.setDate(endDate.getDate() - 7));
+            endDate = new Date()
+            const { transactions, results } = await getTransactionsBetweenDatesAction({ startDate, endDate })
+            transactionTotal = await calculateTransactionTotalAction({ transactions, results }) ?? 0
+        } else if (value === "month") {
+            const date = new Date();
+            const startDate = new Date(date.getFullYear(), date.getMonth(), 1);
+            const endDate = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+            const { transactions, results } = await getTransactionsBetweenDatesAction({ startDate, endDate })
+            transactionTotal = await calculateTransactionTotalAction({ transactions, results }) ?? 0
+        } else if (value === "year") {
+            const currentYear = new Date().getFullYear();
+            const startDate = new Date(currentYear, 0, 1);
+            const endDate = new Date(currentYear, 11, 31);
+            const { transactions, results } = await getTransactionsBetweenDatesAction({ startDate, endDate })
+            transactionTotal = await calculateTransactionTotalAction({ transactions, results }) ?? 0
+        } else {
+            transactionTotal = 0;
+        }
 
-  async function handleChange (
-    event: React.MouseEvent<HTMLElement>,
-    value: string,
-  ) {
-    setAlignment(value);
-    setTable(value)
-  };
+        setTimeTransactionTotal(transactionTotal);
+        setTargetItems()
+        console.log("hello")
+        console.log(targetItems.length)
+    }
 
-  return (
-    <div>
-        <ToggleButtonGroup
-          color="primary"
-          value={alignment}
-          exclusive
-          onChange={handleChange}
-          aria-label="Platform"
-        >
-          <ToggleButton value="week">Last 7-Days</ToggleButton>
-          <ToggleButton value="month">This Month</ToggleButton>
-          <ToggleButton value="year">This Year</ToggleButton>
-        </ToggleButtonGroup>
+    async function handleChange(
+        event: React.MouseEvent<HTMLElement>,
+        value: string,
+    ) {
+        setAlignment(value);
+        setTable(value)
+    };
 
-        <p>£{timeTransactionTotal}</p>
+    return (
+        <div>
+            <ToggleButtonGroup
+                color="primary"
+                value={alignment}
+                exclusive
+                onChange={handleChange}
+                aria-label="Platform"
+            >
+                <ToggleButton value="week">Last 7-Days</ToggleButton>
+                <ToggleButton value="month">This Month</ToggleButton>
+                <ToggleButton value="year">This Year</ToggleButton>
+            </ToggleButtonGroup>
 
-        <table className="divide-y-2 table-fixed">
-          <thead>
-            <tr className="text-left text-1xl">
-              <th className="w-10"></th>
-              <th className="pl-5 text-center w-44">Target Value</th>
-              <th className="px-5 text-center w-44">Actual Spending</th>
-              <th className="px-5 text-center w-44">Difference</th>
-            </tr>
+            <p>£{timeTransactionTotal}</p>
 
-            {/* <VarianceTable transactions={tranactions} /> */}
-          </thead>
-        </table>
+            {/* {targetItems?.map((targetItem) => (
+                <div>
+                  <p>{targetItem.targetName}</p>
+                  <p>{targetItem.actualSpend}</p>
+                </div>
+              ))} */}
 
-        <table className="divide-y-2 table-fixed">
+            {/* {targetItems.length < 0 ? (
+                <p>nothing</p>
+              ) : (
+                targetItems.forEach((targetItem) => {
+                  <div>
+                    <p>{targetItem.targetName}</p>
+                    <p>{targetItem.actualSpend}</p>
+                  </div>
+                })
+              )} */}
+
+            <table className="divide-y-2 table-fixed">
+                <thead>
+                    <tr className="text-left text-1xl">
+                        <th className="w-10"></th>
+                        <th className="pl-5 text-center w-44">Target Value</th>
+                        <th className="px-5 text-center w-44">Actual Spending</th>
+                        <th className="px-5 text-center w-44">Difference</th>
+                    </tr>
+                </thead>
+
+                {targetItems.length < 0 ? (
+                    <p> nothing found</p>
+                ) : (
+                    targetItems?.map((targetItem) => (
+                        <tr key={targetItem.targetName + targetItem.targetType}>
+                            <td>{targetItem.targetName}:</td>
+                            <td>£{targetItem.targetValue.toFixed(2)}</td>
+                            <td>£{targetItem.actualSpend.toFixed(2)}</td>
+                            <td>£Insert Difference Calculation here</td>
+                        </tr>
+                    ))
+                )}
+
+            </table>
+
+            <table className="divide-y-2 table-fixed">
                 <thead>
                     <tr className="text-left text-1xl">
                         <th className="w-10"></th>
@@ -157,10 +188,10 @@ export default function timeButton() {
                         <th className="px-5 text-center w-44">Actual Spending</th>
                         <th className="px-5 text-center w-44">Difference</th>
                     </tr>
-                </thead>  
+                </thead>
                 <tr className="">
                     <td className="text-right font-bold">TargetName:</td>
-                    <td className="text-center">£0</td> 
+                    <td className="text-center">£0</td>
                     <td className="text-center">£0</td>
                     <td className="{ expenseDifferenceColor }">£0</td>
                 </tr>
@@ -171,6 +202,6 @@ export default function timeButton() {
                     <td className="{ expenseDifferenceColor }">£0</td>
                 </tr>
             </table>
-    </div>
-  );
+        </div>
+    );
 }
--- app/variance/page.tsx
+++ app/variance/page.tsx
@@ -5,12 +5,12 @@ import { TargetFilter, getTargets } from "../lib/target-db";
 import { revalidatePath } from 'next/cache'
 import { getTransactionsBetweenDates } from "../lib/transaction-db";
 import { getIncomesBetweenDates } from "../lib/income-db";
-import TimeButton from "../components/variance-button-group"
+import VarianceTimeButton from "../components/variance-button-group"
 
 export default async function Home() {
     const transactionFilter = {}
     const incomeFilter = {}
-    
+
     const targetExpenseFilter: TargetFilter = {
         limit: 50,
         type: "expense"
@@ -20,7 +20,7 @@ export default async function Home() {
         limit: 50,
         type: "income"
     }
-    
+
     let { targets: expenseTargets, results: expenseResults } = await getTargets(targetExpenseFilter)
     let { targets: incomeTargets, results: incomeResults } = await getTargets(targetIncomeFilter)
 
@@ -43,33 +43,33 @@ export default async function Home() {
     const incomeDifferenceColor = textColourClass(incomeDifference)
     const savingDifferenceColor = textColourClass(savingDifference)
 
-    async function action(data: FormData){
+    async function action(data: FormData) {
         "use server"
 
         data.forEach((value, key) => {
-            if(!value || typeof Number(value) !== "number"){
+            if (!value || typeof Number(value) !== "number") {
                 return;
             }
-            
-            updateTargetAction(key, {targetAmount: Number(value)}, "/with-server-actions")
+
+            updateTargetAction(key, { targetAmount: Number(value) }, "/with-server-actions")
         });
 
         //This will refresh the page when a target is set to update the calculated variables.
         revalidatePath('/')
     }
 
-    function textColourClass(value: number){
-        if(value > 0){
+    function textColourClass(value: number) {
+        if (value > 0) {
             return "text-center text-green-500"
-        }else if (value < 0){
+        } else if (value < 0) {
             return "text-center text-red-500"
-        }else {
+        } else {
             return "text-center"
         }
     }
 
-    return(
-        <div className="container mx-auto max-w-screen-2xl p-4"> 
+    return (
+        <div className="container mx-auto max-w-screen-2xl p-4">
 
             <h1 className="text-2xl font-bold my-4">Spending Targets vs Actuals</h1>
 
@@ -81,28 +81,28 @@ export default async function Home() {
                         <th className="px-5 text-center w-44">Actual Spending</th>
                         <th className="px-5 text-center w-44">Difference</th>
                     </tr>
-                </thead>  
+                </thead>
                 <tr className="">
                     <td className="text-right font-bold">Expenses:</td>
-                    <td className="text-center">£{targetMonthlyExpenseTotal.toFixed(2)}</td> 
+                    <td className="text-center">£{targetMonthlyExpenseTotal.toFixed(2)}</td>
                     <td className="text-center">£{actualMonthyExpensesTotal.toFixed(2)}</td>
-                    <td className={ expenseDifferenceColor }>£{expenseDifference.toFixed(2)}</td>
+                    <td className={expenseDifferenceColor}>£{expenseDifference.toFixed(2)}</td>
                 </tr>
                 <tr className="">
                     <td className="text-right font-bold">Income:</td>
                     <td className="text-center">£{targetMonthlyIncomeTotal.toFixed(2)}</td>
                     <td className="text-center">£{actualMonthlyIncomeTotal.toFixed(2)}</td>
-                    <td className={ incomeDifferenceColor }>£{incomeDifference.toFixed(2)}</td>
+                    <td className={incomeDifferenceColor}>£{incomeDifference.toFixed(2)}</td>
                 </tr>
                 <tr className="">
                     <td className="text-right font-bold">Savings:</td>
                     <td className="text-center">£{impliedMonthlySaving.toFixed(2)}</td>
                     <td className="text-center">£{actualMonthlySaving.toFixed(2)}</td>
-                    <td className={ savingDifferenceColor }>£{savingDifference.toFixed(2)}</td>
+                    <td className={savingDifferenceColor}>£{savingDifference.toFixed(2)}</td>
                 </tr>
             </table>
 
-            <TimeButton />
+            <VarianceTimeButton />
 
         </div>
     );
