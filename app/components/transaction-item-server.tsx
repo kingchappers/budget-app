7c8de02c711c903c3309e20aa184a465004bb38f
Sam Chapman <sam.chapman@outlook.com>
2023-11-10 10:39:19 +0000

    added data checking to edit form

--- app/components/transaction-item-server.tsx
+++ app/components/transaction-item-server.tsx
@@ -53,50 +53,54 @@ export const TransactionItem: React.FC<TransactionItemProps> = ({ transaction, c
             items: items,
             notes: notes,
         }
+
+        if (typeof transactionDate !== 'object') {
+            console.log(typeof (transactionDate))
+            console.log(transactionDate)
+            return;
+        }
+
+        if (typeof vendor !== 'string') {
+            console.log(typeof (vendor))
+            console.log(vendor)
+            console.log("vendor")
+            return;
+        }
+
+        if (typeof value !== 'number') {
+            console.log(typeof (value))
+            console.log(value)
+            console.log("value")
+            return;
+        }
+
+        if (typeof categoriesSelection !== 'string') {
+            console.log(typeof (categoriesSelection))
+            console.log(categoriesSelection)
+            console.log("categoriesSelection")
+
+            return;
+        }
+
+        if (typeof items !== 'string') {
+            console.log(typeof (items))
+            console.log(items)
+            console.log("items")
+
+            return;
+        }
+
+        if (typeof notes !== 'string') {
+            console.log(typeof (notes))
+            console.log(notes)
+            console.log("notes")
+
+            return;
+        }
+
         updateTransactionAction(id, update, "/")
         setIsEditingTransaction(false)
     }
-
-    async function action(data: FormData) {
-
-
-        const transactionDate = data.get("pickedDate");
-        if (!transactionDate || typeof transactionDate !== "string") {
-            return;
-        }
-
-        const vendor = data.get("vendor");
-        if (!vendor || typeof vendor !== "string") {
-            return;
-        }
-
-        const value = Number(data.get("value"));
-        if (!value || typeof value !== "number") {
-            return;
-        }
-
-        const category = data.get("categoryCombobox");
-        if (!category || typeof category !== "string") {
-            return;
-        }
-
-        const items = data.get("items");
-        if (typeof items !== "string") {
-            return;
-        }
-
-        const notes = data.get("notes");
-        if (typeof notes !== "string") {
-            return;
-        }
-
-        // Invoke server action to add new transaction
-        // await createTransactionAction({ transactionDate, vendor, value, category, items, notes, userId, path: "/" });
-        console.log("done")
-    }
-
-
-
 
     return (
 
@@ -104,14 +108,14 @@ export const TransactionItem: React.FC<TransactionItemProps> = ({ transaction, c
             {isEditingTransaction ? (
                 <tr>
                     <td className="px-5"><DatePicker initialDate={transaction.transactionDate} selectedDate={transactionDate} setSelectedDate={setTransactionDate} /></td>
-                    <td className="px-5"><input type="text" name="vendor" placeholder="Vendor" defaultValue={transaction.vendor} onChange={() => handleInputFieldChange(this.event, transaction.vendor, transaction.value, transaction.category, transaction.items, transaction.notes)} className="border rounded px-1 py-1 w-52" /></td>
-                    <td className="px-5"><input type="number" step="any" name="value" placeholder="Value" defaultValue={transaction.value} onChange={() => handleInputFieldChange(transaction.vendor, transaction.value, transaction.category, transaction.items, transaction.notes)} className="border rounded px-1 py-1 w-20" /></td>
+                    <td className="px-5"><input type="text" name="vendor" placeholder="Vendor" defaultValue={transaction.vendor} onChange={(event) => handleInputFieldChange(event, transaction.vendor, transaction.value, transaction.category, transaction.items, transaction.notes)} className="border rounded px-1 py-1 w-52" /></td>
+                    <td className="px-5"><input type="number" step="any" name="value" placeholder="Value" defaultValue={transaction.value} onChange={(event) => handleInputFieldChange(event, transaction.vendor, transaction.value, transaction.category, transaction.items, transaction.notes)} className="border rounded px-1 py-1 w-20" /></td>
                     <td className="px-5"><CategoryComboBox categories={categories} categoriesSelection={categoriesSelection} setCategoriesSelection={setCategoriesSelection} /></td>
-                    <td className="px-5"><input type="text" name="items" placeholder="Items" defaultValue={transaction.items} onChange={() => handleInputFieldChange(transaction.vendor, transaction.value, transaction.category, transaction.items, transaction.notes)} className="border rounded px-1 py-1 w-44" /></td>
-                    <td className="px-5"><input type="text" name="notes" placeholder="Notes" defaultValue={transaction.notes} onChange={() => handleInputFieldChange(transaction.vendor, transaction.value, transaction.category, transaction.items, transaction.notes)} className="border rounded px-1 py-1 w-80" /></td>
+                    <td className="px-5"><input type="text" name="items" placeholder="Items" defaultValue={transaction.items} onChange={(event) => handleInputFieldChange(event, transaction.vendor, transaction.value, transaction.category, transaction.items, transaction.notes)} className="border rounded px-1 py-1 w-44" /></td>
+                    <td className="px-5"><input type="text" name="notes" placeholder="Notes" defaultValue={transaction.notes} onChange={(event) => handleInputFieldChange(event, transaction.vendor, transaction.value, transaction.category, transaction.items, transaction.notes)} className="border rounded px-1 py-1 w-80" /></td>
                     {/* <td><TransactionItemMenu transaction={transaction} isEditingTransaction={isEditingTransaction} setIsEditingTransaction={setIsEditingTransaction} /></td> */}
                     <td></td>
-                    <td><button onClick={() => handleClick(transaction.id)} className="px-4 py-1 text-white rounded bg-green-500">Add</button></td>
+                    <td><button onClick={() => handleClick(transaction.id)} className="px-4 py-1 text-white rounded bg-green-500">Save Changes</button></td>
                 </tr>
             ) : (
                 <tr>
@@ -131,25 +135,3 @@ export const TransactionItem: React.FC<TransactionItemProps> = ({ transaction, c
         </tbody>
     );
 };
-
-{/* <form action={action} key={Math.random()} className="flex items-center space-x-3 mb-4">
-            <DatePicker />
-            <input type="text" name="vendor" placeholder="Vendor" className="border rounded px-1 py-1 w-52" />
-            <input type="number" step="any" name="value" placeholder="Value" className="border rounded px-1 py-1 w-20" />
-            <CategoryComboBox categories={categories} />
-            <input type="text" name="items" placeholder="Items" className="border rounded px-1 py-1 w-44" />
-            <input type="text" name="notes" placeholder="Notes" className="border rounded px-1 py-1 w-80" />
-            <button className="px-4 py-1 text-white rounded bg-green-500">Add</button>
-        </form> */}
-
-// <tr>
-//     <td className="px-5">{transactionDateString}</td>
-//     <td className="px-5">{transaction.vendor}</td>
-//     <td className="px-5">£{transaction.value.toFixed(2)}</td>
-//     <td className="px-5">{transaction.category}</td>
-//     <td className="px-5">{transaction.items}</td>
-//     <td className="px-5">{transaction.notes}</td>
-//     <td><TransactionItemMenu transaction={transaction} isEditingTransaction={isEditingTransaction} setIsEditingTransaction={setIsEditingTransaction} /></td>
-//     {/* <td><TransactionItemMenu transaction={transaction} isEditingTransaction={isEditingTransaction} /></td> */}
-//     <td>{isEditingTransaction ? (<p>true</p>) : (<p>false</p>)}</td>
-// </tr>
