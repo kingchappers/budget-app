8056ceffc3b3d9a67d38fe5c8f3a8c7db3b3e2a4
Sam Chapman <sam.chapman@outlook.com>
2023-11-19 15:45:16 +0000

    Moved graph to component module

--- app/trends/page.tsx
+++ app/trends/page.tsx
@@ -1,8 +1,4 @@
-"use client"
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme, VictoryContainer } from 'victory';
+import { YearlySpendTrend } from '../components/trend-graphs';
 
 export default async function Trends() {
     const data = [
@@ -16,22 +12,8 @@ export default async function Trends() {
         <div className="container mx-auto max-w-screen-2xl p-4">
 
             <h1 className="text-2xl font-bold mb-4">Spending and Income Trends</h1>
-            <VictoryContainer>
-            {/* <div className="w-60"> */}
-                <VictoryChart domainPadding={20} theme={VictoryTheme.material} style={{ parent: { maxWidth: "20%" } }}>
-                    <VictoryAxis tickValues={[1, 2, 3, 4]} tickFormat={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]} />
-                    <VictoryAxis dependentAxis tickFormat={(x) => (`$${x / 1000}k`)}
-                    />
-                    <VictoryBar
-                        data={data}
-                        // data accessor for x values
-                        x="quarter"
-                        // data accessor for y values
-                        y="earnings"
-                    />
-                </VictoryChart>
-            {/* </div> */}
-            </VictoryContainer>
+
+            <YearlySpendTrend />
 
         </div>
     );
