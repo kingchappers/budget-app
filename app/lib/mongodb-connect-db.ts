afe1cd62875d21d36d298aaf0d67bcd025f18970
Sam Chapman <sam.chapman@outlook.com>
2023-12-12 06:47:29 +0000

    another theory

--- app/lib/mongodb-connect-db.ts
+++ app/lib/mongodb-connect-db.ts
@@ -4,7 +4,7 @@ import { MongoClient } from "mongodb"
 if (!process.env.REACT_APP_MONGODB_URI) {
     var test = process.env.REACT_APP_NEXTAUTH_URL
     console.log(`${test}`)
-    var test2 = process.env.REACT_APP_MONGODB_URI
+    var test2 = process.env.secrets.REACT_APP_TEST_SECRET
     console.log(`${test2}`)
     throw new Error('Invalid/Missing environment variable: "REACT_APP_MONGODB_URI" this is not auto generated')
 }
