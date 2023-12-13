d7a0114c5c66aef5d9c363af9766e6e00697af62
Sam Chapman <sam.chapman@outlook.com>
2023-12-13 16:03:38 +0000

    testing

--- app/lib/mongoose-connect-db.ts
+++ app/lib/mongoose-connect-db.ts
@@ -9,6 +9,7 @@ declare global {
 }
 
 const MONGODB_URI = secret('REACT_APP_MONGODB_URI').toString();
+console.log("the test secret should be under here:")
 const test = secret('REACT_APP_TEST_SECRET').toString();
 console.log(test)
 
