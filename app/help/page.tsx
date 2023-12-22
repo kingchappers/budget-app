66a5785dc2d9d6f438980bfad592b100f752a883
Sam Chapman <sam.chapman@outlook.com>
2023-12-22 08:46:55 +0000

    Updated help page styles

--- app/help/page.tsx
+++ app/help/page.tsx
@@ -1,8 +1,8 @@
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div className="w-full max-w-5xl items-center justify-between">
+        <main>
+            <div>
                 <h1 className="text-4xl font-extrabold mb-5">Help Page</h1>
                 <p>This page is awaiting content and will be filled as the budget app gains functionality.</p>
             </div>
