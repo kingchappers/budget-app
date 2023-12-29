3d222a86fcbe3b1d6a6e3dfaa3e792707ca6cc8c
Sam Chapman <sam.chapman@outlook.com>
2023-12-29 21:12:11 +0000

    updated to responsive styles

--- app/trends/page.tsx
+++ app/trends/page.tsx
@@ -29,7 +29,7 @@ export default async function Trends() {
 
             <h1 className="text-xl font-bold mb-4">Category Spending Per Month</h1>
 
-            <div className="grid grid-cols-3">
+            <div className="grid grid-cols-1 lg:grid-cols-3">
                 {results === 0 ? (
                     <p>No data found</p>
                 ) : (
@@ -40,7 +40,7 @@ export default async function Trends() {
             </div>
 
             <h1 className="text-xl font-bold mb-4">Category Income Per Month</h1>
-            <div className="grid grid-cols-3">
+            <div className="grid grid-cols-1 lg:grid-cols-3">
                 {results === 0 ? (
                     <p>No data found</p>
                 ) : (
