b2a179c5beb7e4ecacc7ee46fc3acfba13db1c14
Sam Chapman <sam.chapman@outlook.com>
2023-12-30 18:32:56 +0000

    updated profile page to display user info

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -15,32 +15,14 @@ export default async function Profile() {
         redirect("/api/auth/signin");
     }
 
-    const users: User[] = await fetch(
-        "https://jsonplaceholder.typicode.com/users"
-    ).then((res) => res.json());
+    const name = session.user.name;
+    const email = session.user.email;
 
     return (
-        <main style={{ maxWidth: 1200, marginInline: "auto", padding: 20 }}>
-            <div
-                style={{
-                    display: "grid",
-                    gridTemplateColumns: "1fr 1fr 1fr 1fr",
-                    gap: 20,
-                }}
-            >
-                {users.map((user) => (
-                    <div
-                        key={user.id}
-                        style={{ border: "1px solid #ccc", textAlign: "center" }}
-                    >
-                        <img
-                            src={`https://robohash.org/${user.id}?set=set2&size=180x180`}
-                            alt={user.name}
-                            style={{ height: 180, width: 180 }}
-                        />
-                        <h3>{user.name}</h3>
-                    </div>
-                ))}
+        <main>
+            <div>
+                <h1 className="text-4xl font-extrabold">Welcome to your profile page {name}!</h1>
+                <h1 className="text-4xl font-extrabold mb-5">Your current email address is {email}</h1>
             </div>
         </main>
     );
