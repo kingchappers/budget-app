5927689188f8aa75f42f1fd27f64b400c61dc04e
Sam Chapman <sam.chapman@outlook.com>
2024-01-07 12:13:32 +0000

    updated date

--- app/components/form-submit-buttons.tsx
+++ app/components/form-submit-buttons.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-//@ts-expect-error TypeScript error is currently expected until there is a proper update to react-dom (07/01/2024)
+//@ts-expect-error TypeScript error is currently expected until there is a proper update to react-dom - Seventh Jan 2024
 import { useFormStatus } from 'react-dom'
 
 export function FormAddButton() {
