66a6b1e5714ed768cd42343f4c698cf8bbd3ef5c
Sam Chapman <sam.chapman@outlook.com>
2024-03-25 08:31:41 +0000

    updated the total update function - WIP

--- app/components/transaction-form-server.tsx
+++ app/components/transaction-form-server.tsx
@@ -59,13 +59,85 @@ export function TransactionForm({ categories, userId }: TransactionFormProps) {
         const transactionDateMonthStart = startOfMonth(stringToDate(transactionDate))
 
         const { monthlySpend } = await getMonthlySpendByMonthAction({ month: transactionDateMonthStart, userId })
-
-        // console.log(monthlySpend?.monthCategoryTotals[0])
+        // console.log(monthlySpend)
 
         if(monthlySpend){
             monthlySpend.monthCategoryTotals.forEach((monthCategoryTotal) => {
-                // if(monthCategoryTotal.categoryName)
                 console.log(monthCategoryTotal.categoryName)
+
+                console.log("Monthly Spend Total Before update: " + monthlySpend.monthTotal)
+                const monthTotalUpdate = monthlySpend.monthTotal + value
+                console.log("Monthly Spend Total After Update: " + monthTotalUpdate)
+
+                if(monthCategoryTotal.categoryName == category){
+                    console.log("Category match!")
+                    console.log(monthCategoryTotal)
+
+
+
+                    const monthlySpendUpdate = {
+                        monthTotal: monthTotalUpdate,
+                        monthCategoryTotals: []
+                    }
+
+
+
+
+
+
+
+                    // EXAMPLE monthCategoryTotal OBJECT FOR REFERENCE
+                    // {
+                    //     chartTitle: 'boogey:\n£4 | 14.81%',
+                    //     categoryName: 'boogey',
+                    //     value: 4,
+                    //     percentage: 14.814814814814813
+                    //   }
+
+
+
+
+
+                    //GENERATING THE VALUES BELOW
+
+                    // const monthTotal = calculateTransactionTotal(transactions);
+                    // const categorySpendData: categoryData[] = Object.entries(categorySpendRecord).map(([category, value]) => ({
+                    //        chartTitle: category + `:\n£${value} | ${((value / monthTotal) * 100).toFixed(2)}%`,
+                    //        categoryName: category,
+                    //        value: value,
+                    //        percentage: ((value / monthTotal) * 100),
+                    //    }))
+
+
+
+                    // Full object for reference
+                    // {
+                    //     _id: new ObjectId('65ff3be396f195a2effd627f'),
+                    //     month: 2024-03-01T00:00:00.000Z,
+                    //     monthTotal: 27,
+                    //     monthCategoryTotals: [
+                    //       {
+                    //         chartTitle: 'boogey:\n£4 | 14.81%',
+                    //         categoryName: 'boogey',
+                    //         value: 4,
+                    //         percentage: 14.814814814814813
+                    //       },
+                    //       {
+                    //         chartTitle: 'test:\n£23 | 85.19%',
+                    //         categoryName: 'test',
+                    //         value: 23,
+                    //         percentage: 85.18518518518519
+                    //       }
+                    //     ],
+                    //     userId: '65391927de5c11a3e686c2f7',
+                    //     createdAt: 2024-03-23T20:30:27.970Z,
+                    //     updatedAt: 2024-03-23T20:30:27.970Z,
+                    //     __v: 0
+                    //   }
+
+                } else {
+                    // Logic for creating a new entry for monthlyCategoryTotals - updating the database
+                }
             })
         }
     }
