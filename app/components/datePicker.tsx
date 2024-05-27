ab43ce8d1faa5db711789ba20a4b6de078119fa7
Sam Chapman <sam.chapman@outlook.com>
2024-05-27 11:39:00 +0100

    removed react datepicker

--- app/components/datePicker.tsx
+++ app/components/datePicker.tsx
@@ -1,8 +1,6 @@
 "use client"
 
 import React, { Dispatch, SetStateAction } from "react"
-import Datetime from "react-datetime"
-import "react-datetime/css/react-datetime.css"
 import { dateToStringInputFormat, stringToDate, stringToDateInputFormat } from "../lib/utils"
 
 interface DatePickerProps {
@@ -49,26 +47,26 @@ interface DatePickerProps {
 // _________________________________________________________________________________________________________________________________________________________________________
 // _________________________________________________________________________________________________________________________________________________________________________
 
-export function BetweenTwoDates() {
-    let startDateInputProps = {
-        name: 'startDate',
-        size: 8,
-    };
+// export function BetweenTwoDates() {
+//     let startDateInputProps = {
+//         name: 'startDate',
+//         size: 8,
+//     };
 
-    let endDateInputProps = {
-        name: 'endDate',
-        size: 8,
-    };
+//     let endDateInputProps = {
+//         name: 'endDate',
+//         size: 8,
+//     };
 
-    const date = new Date();
-    const startDate = new Date(date.getFullYear(), date.getMonth(), 1);
-    const endDate = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+//     const date = new Date();
+//     const startDate = new Date(date.getFullYear(), date.getMonth(), 1);
+//     const endDate = new Date(date.getFullYear(), date.getMonth() + 1, 0);
 
-    return (
-        <div className="flex items-center">
-            <Datetime dateFormat="DD/MM/YYYY" inputProps={startDateInputProps} initialValue={startDate} timeFormat={false} className="bg-white border rounded px-1 py-1 ml-5" />
-            <p className="mx-2"> to </p>
-            <Datetime dateFormat="DD/MM/YYYY" inputProps={endDateInputProps} initialValue={endDate} timeFormat={false} className="bg-white border rounded px-1 py-1" />
-        </div>
-    )
-}
+//     return (
+//         <div className="flex items-center">
+//             <Datetime dateFormat="DD/MM/YYYY" inputProps={startDateInputProps} initialValue={startDate} timeFormat={false} className="bg-white border rounded px-1 py-1 ml-5" />
+//             <p className="mx-2"> to </p>
+//             <Datetime dateFormat="DD/MM/YYYY" inputProps={endDateInputProps} initialValue={endDate} timeFormat={false} className="bg-white border rounded px-1 py-1" />
+//         </div>
+//     )
+// }
